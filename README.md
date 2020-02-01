47328ee653b3465c3fb1404ac13cdcf15d63dbc2
AMIT VERMA <55926153+amitverma07@users.noreply.github.com>
2020-02-01 15:24:52 -0600

    Initial Build

---
+++ README.md
@@ -0,0 +1,19 @@
+# Top 10 News Website
+Simple website to display top 10 news using cbc-news API.
+
+# Table Of Content
+
+- Funtionalities
+- Technology Used
+
+## Funtionalities
+Click on the button of the given bus route. It will dispaly the information about the upcoming bus on that route.
+
+It fetches the data using API.
+
+![srceen shot](https://raw.githubusercontent.com/amitverma07/Top-10_News_Website/master/readm.PNG)
+
+## Technology Used
+- JavaScript
+- CSS
+- HTML
---
+++ js/main.js
@@ -0,0 +1,42 @@
+let source = 'cbc-news';
+let apikey = '3f07c5f2bbad455dbaf1184d4d0d0b24';
+
+let newsAccordion = document.getElementById('newsAccordion');
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apikey}`, true);
+
+xhr.onload = function() {
+  if(this.status === 200) {
+    let json = JSON.parse(this.responseText);
+    let articles = json.articles;
+
+    let newsHtml = "";
+        articles.forEach(function(element, index) {
+            // console.log(element, index)
+            let news = `<div class="card">
+                            <div class="card-header" id="heading${index}">
+                                <h2 class="mb-0">
+                                <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse${index}"
+                                    aria-expanded="false" aria-controls="collapse${index}">
+                                   <b>Breaking News ${index+1}:</b> ${element["title"]}
+                                </button>
+                                </h2>
+                            </div>
+
+                            <div id="collapse${index}" class="collapse" aria-labelledby="heading${index}" data-parent="#newsAccordion">
+                                <div class="card-body"> ${element["content"]}. <a href="${element['url']}" target="_blank" >Read more here</a>  </div>
+                            </div>
+                        </div>`;
+            newsHtml += news;
+  });
+    newsAccordion.innerHTML = newsHtml;
+  } else {
+    console.log("Some Error Occured");
+  }
+}
+
+xhr.send();
+
+
+
